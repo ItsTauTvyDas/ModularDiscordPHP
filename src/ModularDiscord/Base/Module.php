bc8c6b7f38b941569dbbc801b73f5bc085816ba8
Tautvydas <tautvydas.gaming@gmail.com>
2023-07-16 22:57:44 +0300

    made registry not global

--- src/ModularDiscord/Base/Module.php
+++ src/ModularDiscord/Base/Module.php
@@ -5,14 +5,18 @@ namespace ModularDiscord\Base;
 use Discord\Discord;
 use Discord\WebSockets\Event;
 use ModularDiscord\ModularDiscord;
+use ModularDiscord\Registry;
 use Psr\Log\LoggerInterface;
 
 class Module
 {
     public readonly ModularDiscord $modularDiscord;
     public readonly LoggerInterface $logger;
+    public readonly Registry $registry;
     public readonly string $name, $path;
     private bool $disabled = false;
+
+    public bool $callReadyOnEnable = false;
 
     public function __construct(string $name, string $path, ModularDiscord $modularDiscord)
     {
@@ -20,21 +24,29 @@ class Module
         $this->modularDiscord = $modularDiscord;
         $this->path = $path;
         $this->logger = $modularDiscord->createLogger('Module#'.$name);
+        $this->registry = new Registry($modularDiscord, $this);
     }
 
-    public function onEnable(bool $firstLoad = false) {}
+    public function onEnable() {}
     public function onDisable() {}
     public function onClose(bool $unexpected = false) {}
     public function onDiscordInit(Discord $discord) {}
     public function onDiscordReady(Discord $discord) {}
 
+    public final function isFirstTimeLoad(): bool
+    {
+        return !isset($this->modularDiscord->getModules()[$this->name]);
+    }
+
     public final function setEnabled(bool $bool)
     {
         $this->disabled = !$bool;
-        if ($bool and $this->disabled)
+        if ($bool) {
             $this->onEnable();
-        else if (!$bool and !$this->disabled)
+        } else {
             $this->onDisable();
+            $this->registry->removeDiscordListeners();
+        }
     }
 
     public final function isEnabled(): bool
--- src/ModularDiscord/Registry.php
+++ src/ModularDiscord/Registry.php
@@ -11,40 +11,64 @@ use ReflectionMethod;
 
 final class Registry
 {
-    private function __construct() {}
+    private readonly ModularDiscord $modularDiscord;
+    private readonly Module $module;
+    private array $listeners = [];
 
-    public static function registerListener(Module $module, Discord $discord, Listener $listener, array &$listeners = [])
+    public function __construct(ModularDiscord $modularDiscord, Module $module)
+    {
+        $this->modularDiscord = $modularDiscord;
+        $this->module = $module;
+    }
+
+    public function registerListener(Listener $listener, array &$listeners = [])
     {
         $methods = get_class_methods($listener);
 
+        $count = 0;
         foreach ($methods as $method)
             if (str_starts_with($method, 'on')) {
                 $name = substr($method, 2);
                 $reflection = new ReflectionClass('Discord\WebSockets\Event');
                 $refMethod = new ReflectionMethod($listener, $method);
-                $constants = $reflection->getConstants();
-                $event = null;
-                foreach ($constants as $constant)
-                    if (str_replace('_', '', strtolower($constant)) == strtolower($name)) {
-                        $event = $reflection->getConstant($constant);
-                        break;
-                    }
-                if ($event != null) {
-                    $discord->on($event, $closure = $refMethod->getClosure($listener));
+                $event = current(array_filter($reflection->getConstants(), fn ($const) => str_replace('_', '', strtolower($const)) == strtolower($name)));
+                if (is_string($event)) {
+                    $this->modularDiscord->discord->on($event, $closure = $refMethod->getClosure($listener));
                     $listeners[$event] = $closure;
+                    $this->listeners[$event][] = $closure;
+                    $count++;
                     continue;
                 }
-                //$module->logger->warning("Couldn't find event that is close to " . ucfirst($name));
             }
 
-        $c = count($listeners);
-        $module->logger->info("Registered $c listener" . ($c > 1 ? 's' : ''), [
-            'listeners' => array_keys($listeners)
+        $this->module->logger->info("Registered $count listener" . ($count > 1 ? 's' : ''), [
+            'listeners' => array_keys($listeners),
+            'class' => get_class($listener)
         ]);
     }
 
-    public static function registerCommand(Module $module, Discord $discord, Command $command)
+    public function removeDiscordListeners()
+    {
+        $count = 0;
+        foreach ($this->listeners as $key => $value) {
+            foreach ($value as $listener) {
+                $this->modularDiscord->discord->removeListener($key, $listener);
+            }
+        }
+        if ($count)
+            $this->module->logger->info("Unegistered $count listener" . ($count > 1 ? 's' : ''), [
+                'listeners' => array_keys($this->listeners),
+                'class' => get_class($listener)
+            ]);
+    }
+
+    public function registerCommand(Command $command)
     {
 
+    }
+
+    public function getListeners(): array
+    {
+        return $this->listeners;
     }
 }
