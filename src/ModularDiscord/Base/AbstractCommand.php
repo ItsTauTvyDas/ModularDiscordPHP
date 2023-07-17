8cb08458e013e7001134e13ebb467c4a205a8112
Tautvydas <tautvydas.gaming@gmail.com>
2023-07-17 13:12:48 +0300

    rename

---
+++ src/ModularDiscord/Base/AbstractCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ModularDiscord\Base;
+
+use Discord\Builders\CommandBuilder;
+use Discord\Discord;
+use Discord\Parts\Interactions\Interaction;
+use Discord\Repository\Interaction\OptionRepository;
+use ModularDiscord\ModularDiscord;
+
+abstract class AbstractCommand
+{
+    public readonly ModularDiscord $modularDiscord;
+    public readonly Discord $discord;
+    public readonly Module $module;
+
+    public function __construct(Module $module) {
+        $this->module = $module;
+        $this->modularDiscord = $module->modularDiscord;
+        $this->discord = $module->modularDiscord->discord;
+    }
+
+    public abstract function onCreate(): CommandBuilder;
+    public abstract function onCommand(Interaction $interaction, OptionRepository $args);
+    public function onAutoComplete(Interaction $interaction, OptionRepository $args): array {return []; }
+    public function getGuild(): ?string { return null; }
+}
