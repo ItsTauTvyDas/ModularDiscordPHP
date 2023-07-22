7859a3ae3f7e5bf17a891d19646a76f19bfadce8
Tautvydas <tautvydas.gaming@gmail.com>
2023-07-22 17:59:12 +0300

    new console commands

--- src/ModularDiscord/InteractableConsole.php
+++ src/ModularDiscord/InteractableConsole.php
@@ -4,6 +4,7 @@ namespace ModularDiscord;
 
 use Error;
 use Exception;
+use ParseError;
 use Seld\Signal\SignalHandler;
 
 class InteractableConsole
@@ -38,7 +39,7 @@ class InteractableConsole
                     $args = [];
                     if (count($split) > 1)
                         $args = array_slice($split, 1);
-                    $callable = self::$registry[$name];
+                    $callable = self::$registry[$name] ?? null;
                     if ($callable == null)
                         return self::handleDefaultCommand($modDiscord, $name, $args);
                     $callable($modDiscord, $name, $args);
@@ -79,19 +80,74 @@ class InteractableConsole
         });
     }
 
-    public static function handleDefaultCommand(ModularDiscord $modDiscord, string $name, array $args)
+    private static function handleDefaultCommand(ModularDiscord $modDiscord, string $name, array $args)
     {
+        $log = $modDiscord->discord->getLogger();
         switch ($name) {
             case 'stop':
             case 'close':
             case 'end':
-                $modDiscord->close();
-                break;
+                return$modDiscord->close();
+                return;
+            case 'help':
+                $log->info('fetch');
+                $log->info('uptime');
+                $log->info('stop/close/end');
+                $log->info('forceexit');
+                $log->info('reloadmodule/rlmod <name>');
+                $log->info('callaccessor <name>');
+                $log->info('callmodule <name>');
+                $log->info('refreshmodule/rfmod <name>');
+                return;
+            case 'refreshmodule':
+            case 'rfmod':
+                if (isset($args[0])) {
+                    if (!$modDiscord->refreshModuleFile($args[0], $newNameRef)) {
+                        if (!isset($newNameRef))
+                            return $log->error("Module '{$args[0]}' doesn't exist!");
+                        return;
+                    }
+                    return $log->info("Module '{$args[0]}' successfully refreshed/reloaded!");
+                }
+                return $log->error('Missing args');
+            case 'reloadmodule':
+            case 'rlmod':
+                if (isset($args[0])) {
+                    if ($modDiscord->reloadModule($args[0]))
+                        return $log->info("Module '{$args[0]}' reloaded successfully.");
+                    return $log->error("Module '{$args[0]}' doesn't exist!");
+                }
+                return $log->error('Missing args');
+            case 'callaccessor':
+            case 'callmodule':
+                $type = substr($name, 4);
+                if (isset($args[0])) {
+                    if (($mod = $modDiscord->$type($args[0])) != null) {
+                        $function = $args[1] ?? 'consoleCall';
+                        if (!method_exists($mod, $function))
+                            return $log->error("Function '$function' doesn't exist!");
+                        try {
+                        return $mod->$function();
+                        } catch (Exception | Error | ParseError $e) {
+                            $modDiscord->handleException($e, "Got an exception while invoking \${$type}->{$function}() !");
+                        }
+                        return;
+                    }
+                    return $log->info(ucfirst($type) . " '{$args[0]}' doesn't exist!");
+                }
+                return $log->error('Missing args');
+            case 'callmodule':
+            case 'fetch':
+                switch ($args[0] ?? null) {
+                    default:
+                        $log->info('> fetch command <name/id>:<[name/id]> <[method/field]/print/methods/fields>');
+                        $log->info('> fetch <user/guild/member> <[value]> <[method/field]/print/methods/fields>');
+                }
+                return;
             case 'forceexit':
-                exit(-1);
-                break;
+                return exit(-1);
             default:
-                $modDiscord->discord->getLogger()->info("Invalid command: $name");
+                return $log->error("Invalid command: $name");
         }
     }
 
